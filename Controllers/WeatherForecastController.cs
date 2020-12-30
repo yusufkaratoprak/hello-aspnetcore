665f04626b5229452945d70efc29ea7939fc5b2a
yusuf karatoprak <yusufkaratoprak@yahoo.com>
2020-12-30 19:45:04 +0300

    second

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,5 +35,19 @@ namespace HelloAspNetCore.Controllers
             })
             .ToArray();
         }
+
+          [HttpGet]
+        public IEnumerable<WeatherForecast> GetById(int id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
     }
 }
